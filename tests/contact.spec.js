297a7b1461430668ded3a7402e5ef7892b7d80fb
SakshyamStha <sakshamstha6785@gmail.com>
2024-08-29 21:56:55 +0545

    some codes

--- tests/contact.spec.js
+++ tests/contact.spec.js
@@ -1,6 +1,7 @@
 import{test,expect} from '@playwright/test';
 import { LoginPage } from '../pageObjects/login.po';
 import { AddContact } from '../pageObjects/addcontact.po';
+import { EditContact } from '../pageObjects/editcontact.po';
 const testData=require('../fixtures/loginFixture.json');
 const testData1=require('../fixtures/contactfixtures.json');
 
@@ -41,5 +42,32 @@ test.describe('to see if the new contact is added or not',()=>{
             testData1.add.lname,
             testData1.add.dob);
     })
+})
+
+test.only('Contact edit Test', async ({page,request})=>{
+    await page.locator('//td[contains(text(),"saxam@gmail.com")]').click();
+    const Data = {
+        "fname":"sakshyam",
+        "lname":"shrestha",
+        "dob":"1999-08-11",
+        "email":"saxam@gmail.com",
+        "phone":"1234567898",
+        "adr1":"ktm",
+        "adr2":"pkr",
+        "city":"ktm",
+        "state":"04",
+        "postal":"11111",
+        "country":"nepal"
+    };
+    const contact = new EditContact(page);
+    accessToken = await authenticateUser(testData.validUser.userName,testData.validUser.password);
+    await createEntity(Data, accessToken,'/contacts', (request));
+    page.reload();
+    await contact.viewContact();
+    await contact.contactEdit(testData1.edit.fname);
+
+    const id = await getEntity(accessToken,'/contacts','200',(request));
+    await deleteEntity(accessToken, /contacts/(id),(request));
+    await validateEntry(accessToken), /contacts/(id),'404',(request);
 })
 
